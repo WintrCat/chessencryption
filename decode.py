fe525291b7f4a8daebf3ba4abc394f448e59b1dc
WintrCat <wintrcat.wilson@gmail.com>
2024-09-05 21:32:44 +0100

    initial

---
+++ decode.py
@@ -0,0 +1,85 @@
+from time import time
+from math import log2
+from chess import pgn, Board
+from util import get_pgn_games
+
+
+###
+### Pass in a PGN string of 1 or more games
+### and also the file path that it should write to in the end
+###
+def decode(pgn_string: str, output_file_path: str):
+    start_time = time()
+
+    total_move_count = 0
+
+    # load games from pgn file
+    games: list[pgn.Game] = get_pgn_games(pgn_string)
+
+    # convert moves to binary and write to output file
+    with open(output_file_path, "w") as output_file:
+        output_file.write("")
+
+    output_file = open(output_file_path, "ab")
+    output_data = ""
+
+    for game_index, game in enumerate(games):
+        chess_board = Board()
+
+        game_moves = list(game.mainline_moves())
+        total_move_count += len(game_moves)
+
+        for move_index, move in enumerate(game_moves):
+            # get UCIs of legal moves in current position
+            legal_move_ucis = [
+                legal_move.uci()
+                for legal_move in list(chess_board.generate_legal_moves())
+            ]
+
+            # get binary of the move played, using its index in the legal moves
+            move_binary = bin(
+                legal_move_ucis.index(move.uci())
+            )[2:]
+
+            # if this is the last move of the last game,
+            # binary cannot go over a total length multiple of 8
+            if (
+                game_index == len(games) - 1 
+                and move_index == len(game_moves) - 1
+            ):
+                max_binary_length = min(
+                    int(log2(
+                        len(legal_move_ucis)
+                    )),
+                    8 - (len(output_data) % 8)
+                )
+            else:
+                max_binary_length = int(log2(
+                    len(legal_move_ucis)
+                ))
+
+            # Pad move binary to meet max binary length
+            required_padding = max(0, max_binary_length - len(move_binary))
+            move_binary = ("0" * required_padding) + move_binary
+
+            # play move on board
+            chess_board.push_uci(move.uci())
+
+            # add move binary to output data string
+            output_data += move_binary
+
+            # if output binary pool is multiple of 8, flush it to file
+            if len(output_data) % 8 == 0:
+                output_file.write(
+                    bytes([
+                        int(output_data[i * 8 : i * 8 + 8], 2)
+                        for i in range(len(output_data) // 8)
+                    ])
+                )
+                output_data = ""
+
+    print(
+        "\nsuccessfully decoded pgn with "
+        + f"{len(games)} game(s), {total_move_count} total move(s)"
+        + f"({round(time() - start_time, 3)}s)."
+    )
---
+++ encode.py
@@ -0,0 +1,101 @@
+from time import time
+from math import log2
+from chess import pgn, Board
+from util import to_binary_string
+
+
+###
+### Enter a file path
+### and it returns a string of 1 or more PGNs that represent it
+###
+def encode(file_path: str):
+    start_time = time()
+
+    # read binary of file
+    print("reading file...")
+
+    file_bytes = list(open(file_path, "rb").read())
+
+    # record number of bits in file
+    file_bits_count = len(file_bytes) * 8
+
+    # convert file to chess moves
+    print("\nencoding file...")
+
+    output_pgns: list[str] = []
+
+    file_bit_index = 0
+
+    chess_board = Board()
+
+    while True:
+        legal_moves = list(chess_board.generate_legal_moves())
+
+        # assign moves a binary value based on its index
+        move_bits = {}
+
+        max_binary_length = min(
+            int(log2(
+                len(legal_moves)
+            )),
+            file_bits_count - file_bit_index
+        )
+
+        for index, legal_move in enumerate(legal_moves):
+            move_binary = to_binary_string(index, max_binary_length)
+            if len(move_binary) > max_binary_length:
+                break
+
+            move_bits[legal_move.uci()] = move_binary
+
+        # take next binary chunk from the file
+        closest_byte_index = file_bit_index // 8
+
+        file_chunk_pool = "".join([
+            to_binary_string(byte, 8)
+            for byte in file_bytes[closest_byte_index : closest_byte_index + 2]
+        ])
+
+        next_file_chunk = file_chunk_pool[
+            file_bit_index % 8
+            : file_bit_index % 8 + max_binary_length
+        ]
+
+        # push chess move that corresponds with next chunk
+        for move_uci in move_bits:
+            move_binary = move_bits[move_uci]
+
+            if move_binary == next_file_chunk:
+                chess_board.push_uci(move_uci)
+                break
+
+        # move the pointer along by the chunk size
+        file_bit_index += max_binary_length
+
+        # check if the game is in a terminal state or EOF
+        # if it is, convert it to a pgn and add to pgn list
+        eof_reached = file_bit_index >= file_bits_count
+
+        if (
+            chess_board.legal_moves.count() <= 1
+            or chess_board.is_insufficient_material()
+            or chess_board.can_claim_draw()
+            or eof_reached
+        ):
+            pgn_board = pgn.Game()
+            pgn_board.add_line(chess_board.move_stack)
+
+            output_pgns.append(str(pgn_board))
+
+            chess_board.reset()
+
+        if eof_reached: break
+
+    print(
+        f"\nsuccessfully converted file to pgn with "
+        + f"{len(output_pgns)} game(s) "
+        + f"({round(time() - start_time, 3)}s)."
+    )
+
+    # return pgn string
+    return "\n\n".join(output_pgns)
