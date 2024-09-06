92a1cb8d4b75830f8ec729cb63bb54a46aac9678
joanferrecid098 <6hpd15h0-github@tigelcid.es>
2024-09-07 01:20:17 +0200

    fix: update package version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("readme.md", "r") as f:
 
 setup(
     name='chessencryption',
-    version='1.0',
+    version='1.1',
     packages=find_packages(),
     install_requires=[
         'chess',
